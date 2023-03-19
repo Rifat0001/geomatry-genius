555e7df40e68a3e2dc52c52adee3fe85274587d6
Rifat0001 <mahmudrifat764@gmail.com>
2023-03-19 22:48:46 +0600

    hover effect added by js

---
+++ js/app.js
@@ -0,0 +1,31 @@
+//Function for add hover background change 
+// for triangle
+function changeStyle1() {
+    let element = document.getElementById("background1");
+    element.style.backgroundColor = 'rgb(' + Math.round(Math.random() * 255) + ',' + Math.round(Math.random() * 255) + ',' + Math.round(Math.random() * 255) + ')';
+}
+// for rectangle
+function changeStyle2() {
+    let element = document.getElementById("background2");
+    element.style.backgroundColor = 'rgb(' + Math.round(Math.random() * 255) + ',' + Math.round(Math.random() * 255) + ',' + Math.round(Math.random() * 255) + ')';
+}
+// for parallelogram
+function changeStyle3() {
+    let element = document.getElementById("background3");
+    element.style.backgroundColor = 'rgb(' + Math.round(Math.random() * 255) + ',' + Math.round(Math.random() * 255) + ',' + Math.round(Math.random() * 255) + ')';
+}
+// for rhombus
+function changeStyle4() {
+    let element = document.getElementById("background4");
+    element.style.backgroundColor = 'rgb(' + Math.round(Math.random() * 255) + ',' + Math.round(Math.random() * 255) + ',' + Math.round(Math.random() * 255) + ')';
+}
+// for pentagon
+function changeStyle5() {
+    let element = document.getElementById("background5");
+    element.style.backgroundColor = 'rgb(' + Math.round(Math.random() * 255) + ',' + Math.round(Math.random() * 255) + ',' + Math.round(Math.random() * 255) + ')';
+}
+// for ellipse
+function changeStyle6() {
+    let element = document.getElementById("background6");
+    element.style.backgroundColor = 'rgb(' + Math.round(Math.random() * 255) + ',' + Math.round(Math.random() * 255) + ',' + Math.round(Math.random() * 255) + ')';
+}
